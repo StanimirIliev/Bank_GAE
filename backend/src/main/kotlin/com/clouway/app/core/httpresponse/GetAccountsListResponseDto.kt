d3baff508b07a6a65f6f3a781ec2be070c850cb5
Stanimir Iliev <stambeto2197@gmail.com>
2018-03-06 12:02:39 +0200

    rebased

--- backend/src/main/kotlin/com/clouway/app/core/httpresponse/GetAccountsListResponseDto.kt
+++
@@ -1,6 +0,0 @@
-package com.clouway.app.core.httpresponse
-
-import com.clouway.app.core.Account
-import java.io.Serializable
-
-data class GetAccountsListResponseDto(val content: List<Account>): Serializable
